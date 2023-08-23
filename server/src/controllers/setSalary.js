417ec957ab4ba61f1cb14bab5704f64f6fed19c0
Febry Dharmawan Junior <febrydj99@gmail.com>
2023-08-23 12:16:24 +0700

    fix set salary

--- server/src/controllers/setSalary.js
+++ server/src/controllers/setSalary.js
@@ -12,13 +12,32 @@ exports.setSalary = async (req, res) => {
       return res.status(404).json({ message: "User not found" });
     }
 
-    await Salary.update(
-      {
-        perhour_salary,
-        monthly_salary,
-      },
-      { where: { user_id: user.user_id }, transaction: t }
-    );
+    let salary = await Salary.findOne({ where: { user_id: user.user_id } });
+
+    if (!salary) {
+      salary = await Salary.create(
+        {
+          user_id: user.user_id,
+          perhour_salary: perhour_salary !== undefined ? perhour_salary : null,
+          monthly_salary: monthly_salary !== undefined ? monthly_salary : null,
+        },
+        { transaction: t }
+      );
+    } else {
+      await Salary.update(
+        {
+          perhour_salary:
+            perhour_salary !== undefined
+              ? perhour_salary
+              : salary.perhour_salary,
+          monthly_salary:
+            monthly_salary !== undefined
+              ? monthly_salary
+              : salary.monthly_salary,
+        },
+        { where: { user_id: user.user_id }, transaction: t }
+      );
+    }
 
     await t.commit();
 
